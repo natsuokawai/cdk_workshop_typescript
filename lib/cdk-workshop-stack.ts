da35a80e0cb5a277c80c6ceec884545a7739e51a
natsuokawai <kawai.natsuo@gmail.com>
2020-06-20 15:56:22 +0900

    add hit count viewer

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
+
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -20,5 +22,10 @@ export class CdkWorkshopStack extends cdk.Stack {
     new apigw.LambdaRestApi(this, 'Endgpoint', {
       handler: helloWithCounter.handler
     });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
+    })
   }
 }
